5806e60fc734284e5295e44c9cb668e6e280a90c
Beccamak <makindeoluwafunmilayo038@gmail.com>
2023-02-28 21:07:51 +0100

    code fix

--- src/middlewares/setupProxy.js
+++ src/middlewares/setupProxy.js
@@ -1,12 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://total-beauty-affairs-backend.onrender.com/',
-      changeOrigin: true,
-    })
-  )
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'https://total-beauty-affairs-backend.onrender.com/',
+//       changeOrigin: true,
+//     })
+//   )
 
-}
+// }
